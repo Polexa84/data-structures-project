666ab925faf1ece52c24391dfe910ba8353bbb5d
polexa84 <polexa84@gmail.com>
2025-01-28 23:34:32 +0300

    Реализованы тесты на unittest

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,33 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+"""Тесты с использованием unittest для модуля stack."""
+from src.stack import Node, Stack
+
+class NodeTest(unittest.TestCase):
+
+    def test_init(self):
+       node = Node(10, 2)
+       self.assertEqual(node.data,10)
+       self.assertEqual(node.next_node, 2)
+
+class StackTest(unittest.TestCase):
+
+    def test_push(self):
+        stack = Stack()
+        stack.push('Hello Word!')
+        self.assertEqual(stack.top.data,'Hello Word!')
+        self.assertEqual(len(stack.top.data),11)
+
+    def test_pop(self):
+        stack = Stack()
+        with self.assertRaises(IndexError):
+            stack.pop()
+        stack.push(10)
+        stack.push(15)
+        stack.push(30)
+        self.assertEqual(stack.top.next_node.data,15)
+        self.assertEqual(stack.top.next_node.next_node.data,10)
+
+
+
+
+
