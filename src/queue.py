84a27ec6e6b5651f2c51e9ad11f618ad486fe5dc
polexa84 <polexa84@gmail.com>
2025-02-04 02:26:05 +0300

    реализовано   - класс `Node`   - методы `__init__`, `enqueue` и `__str__` класса `Queue`

--- src/queue.py
+++ src/queue.py
@@ -1,13 +1,14 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Queue:
@@ -15,7 +16,8 @@ class Queue:
 
     def __init__(self):
         """Конструктор класса Queue"""
-        pass
+        self.head = None
+        self.tail = None
 
     def enqueue(self, data):
         """
@@ -23,7 +25,13 @@ class Queue:
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
+        new_node = Node(data)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def dequeue(self):
         """
@@ -31,8 +39,21 @@ class Queue:
 
         :return: данные удаленного элемента
         """
-        pass
+        if self.head is None:
+            return None  # Или можно поднять исключение
+        data = self.head.data
+        self.head = self.head.next_node
+        if self.head is None:
+            self.tail = None  # Если удалили единственный элемент
+        return data
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
-        pass
+        if self.head is None:
+            return ""
+        items = []
+        current = self.head
+        while current:
+            items.append(str(current.data))
+            current = current.next_node
+        return "\n".join(items)
